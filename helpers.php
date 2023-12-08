b09ad2b3245a67667e2884718ef2bd917ea6b18f
Elvan Hidayat <elvanhd@gmail.com>
2023-12-08 07:39:29 +0700

    inspect helper functions

--- helpers.php
+++ helpers.php
@@ -29,6 +29,7 @@ function loadView($name)
   }
 }
 
+
 /**
  * Load a partial
  *
@@ -45,4 +46,30 @@ function loadPartial($name)
   } else {
     echo "Partial '{$name} not found!'";
   }
+}
+
+/**
+ * Inspect a value(s)
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ *
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
 }
