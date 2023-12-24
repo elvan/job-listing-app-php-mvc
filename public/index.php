67507994326a9ece441d3006927899da8030aba9
Elvan Hidayat <elvanhd@gmail.com>
2023-12-25 00:45:35 +0700

    database class and connection

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 
 require '../helpers.php';
+require basePath('Database.php');
+
+$config = require basePath('config/db.php');
+
+$db = new Database($config);
 
 require basePath('Router.php');
 
