54cda3215315ed60ab33dfd54defdb07fb27cfae
Elvan Hidayat <elvanhd@gmail.com>
2023-12-25 02:07:55 +0700

    folder refactor and custom autoloader

--- public/index.php
+++ public/index.php
@@ -2,8 +2,12 @@
 
 require '../helpers.php';
 
-require basePath('Router.php');
-require basePath('Database.php');
+spl_autoload_register(function ($class) {
+    $path = basePath('Framework/' . $class . '.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
 
 // Instatiate the router
 $router = new Router();
