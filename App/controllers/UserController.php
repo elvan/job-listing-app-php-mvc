f064686027ff98ac531711214c3a41cdf2e3a5a4
Elvan Hidayat <elvanhd@gmail.com>
2023-12-25 20:05:47 +0700

    register and login controller and views

---
+++ App/controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+
+class UserController
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    /**
+     * Show the login page
+     *
+     * @return void
+     */
+    public function login()
+    {
+        loadView('users/login');
+    }
+
+    /**
+     * Show the register page
+     *
+     * @return void
+     */
+    public function create()
+    {
+        loadView('users/create');
+    }
+}
