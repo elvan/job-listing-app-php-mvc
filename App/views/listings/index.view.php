5d871a7f9dc53e4d9255ad6869bf7e49a94db57e
Elvan Hidayat <elvanhd@gmail.com>
2023-12-25 12:02:22 +0700

    handling route params

--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -2,6 +2,7 @@
 <?= loadPartial('navbar') ?>
 <?= loadPartial('top-banner') ?>
 
+
 <!-- Job Listings -->
 <section>
     <div class="container mx-auto p-4 mt-4">
@@ -25,7 +26,7 @@
                                 <strong>Tags:</strong> <?= $listing->tags ?>
                             </li>
                         </ul>
-                        <a href="/listing?id=<?= $listing->id ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+                        <a href="/listing/<?= $listing->id ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
                             Details
                         </a>
                     </div>
